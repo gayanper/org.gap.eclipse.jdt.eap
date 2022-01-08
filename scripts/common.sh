6e0579c6ebf7f842946bc088006a2836f6c36b2d
Gayan Perera <gayanper@gmail.com>
2022-01-08 20:43:49 +0100

    improve script to support last line reading

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,7 +2,7 @@
 function read_repos {
     read_repos_val=()
     local file_path=$1
-    while IFS='=' read -r key value
+    while IFS='=' read -r key value || [ -n "$key" ]
     do
         if [[ $key != \#* ]]; then
             read_repos_val+=("$key:$value")
@@ -13,7 +13,7 @@ function read_repos {
 function read_refs {
     read_refs_val=()
     local file_path=$1
-    while IFS= read -r ref
+    while IFS= read -r ref || [ -n "$ref" ]
     do
         read_refs_val+=("$ref")
     done < "$file_path"
