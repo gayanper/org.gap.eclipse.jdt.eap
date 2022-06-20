1d2cd5f6a8f98361c1b489b964e6bc8b2ffdc7ef
Gayan Perera <gayanper@gmail.com>
2022-06-20 20:31:13 +0200

    Fix pr merge script

--- scripts/merge-prs.sh
+++ scripts/merge-prs.sh
@@ -2,7 +2,7 @@
 function cherry_pick_pr {
     echo "Applying PR # : $1"
 
-    gh pr diff --patch PRNUMBER | git am .
+    gh pr diff --patch $1 | git am .
 
     if [[ $? -ne 0 ]]; then
         # try to reset test code and see if the conflicts go away
